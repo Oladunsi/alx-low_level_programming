c61a1137504b7a248905aa0009087feae43e06ff
Oladunsi <okeoladunsi6@gmail.com>
2022-10-13 21:54:16 -0700

    printing with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+
+* This returns 0 when no errors
+
+* and prints out Programming is like building a multilingual puzzle
+
+*/
+int main(void){
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
