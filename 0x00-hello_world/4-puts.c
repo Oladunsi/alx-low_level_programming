4021a5248f99cbc27bb91bb4dba72a283ce015d1
Oladunsi <okeoladunsi6@gmail.com>
2022-10-13 22:07:39 -0700

    removed the trailing \n in puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
-/*
-
+/**
 * This returns 0 when no errors
-
 * and prints out Programming is like building a multilingual puzzle
-
 */
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle\n");
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
