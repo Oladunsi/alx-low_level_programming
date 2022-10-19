784f5bad1717cd96c671c07193c91c62947a8447
Oladunsi <okeoladunsi6@gmail.com>
2022-10-19 06:56:32 -0700

    documenting 2-print_alphabet_x10 well documented

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* print_alphabet - prints the alphabets in lowercase followed by a new line
+* print_alphabet_x10 - prints the alphabets in lowercase followed by a new line
 *
 * Return: Nothing.
 */
