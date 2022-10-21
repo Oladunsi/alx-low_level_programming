f5e69a3314cdc79cd7243866dc444891543f0bf0
Oladunsi <okeoladunsi6@gmail.com>
2022-10-20 18:14:29 -0700

    printing numbers function

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_numbers - prints out 0-9
+*
+* Return: Nothing
+*/
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+		_putchar(a);
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,3 +2,5 @@ int _putchar(char c);
 int _isupper(char letter);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
+
