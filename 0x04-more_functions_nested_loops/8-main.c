608fbd3820b339c7180f583c470f96499774ec40
Oladunsi <okeoladunsi6@gmail.com>
2022-10-21 09:27:32 -0700

    printing squares

---
+++ 0x04-more_functions_nested_loops/8-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*
+* main - prints numbers btw 0-9 using _putchar function
+* 
+* Return: Always 0 when succesfull
+*
+*/
+
+int main(void)
+
+{
+
+print_square(2);
+print_square(10);
+print_square(0);
+return (0);
+
+}
