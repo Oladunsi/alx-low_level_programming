1f23dcab03926ac307428ceb8bc1c2ff2b5bce55
Oladunsi <okeoladunsi6@gmail.com>
2022-10-22 01:05:51 -0700

    making FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+* main - prints fizz: 3 or buzz: 5 and fizzbuzz: 15
+*
+* Return: 0 when successful
+*/
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+		{
+		if ((a % 3 == 0) && (a % 5 == 0))
+		{
+			printf("Fizz Buzz");
+			printf(" ");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz");
+			printf(" ");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz");
+			printf(" ");
+		}
+		else
+		{
+			printf("%d", a);
+			printf(" ");
+		}
+		}
+	return (0);
+}
