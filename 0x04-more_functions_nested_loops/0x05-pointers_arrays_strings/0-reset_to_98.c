3af67a6ad198daed7e5fa0d8a04e988f290dfb03
Oladunsi <okeoladunsi6@gmail.com>
2022-10-24 20:43:33 -0700

    intial work with pointers in cprogramming language

---
+++ 0x04-more_functions_nested_loops/0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - changes the value in the paramter location
+ * @*n: parameter pointer variable value to be changed
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
+
+
---
+++ 0x04-more_functions_nested_loops/0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Ponters in C
