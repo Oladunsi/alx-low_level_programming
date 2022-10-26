1f3326ed79ea59ef353412565fd7dfd672712036
Oladunsi <okeoladunsi6@gmail.com>
2022-10-25 22:07:53 -0700

    reversing str characters

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints str charaters in reverse
+ * @str: parameter
+ *
+ * Return: Nothing.
+ */
+
+void print_rev(char *str)
+{
+	int n;
+
+	n = 0;
+	while (str[n] != '\0')
+	{
+		n++;
+	}
+
+	while (n >= 0)
+        {
+		_putchar(str[n]);
+                n--;
+        }
+	_putchar('\n');
+}
