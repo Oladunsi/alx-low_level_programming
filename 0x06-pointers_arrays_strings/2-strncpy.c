895d513829bb154c4b56c9819cc0d0624ab68ed2
Oladunsi <okeoladunsi6@gmail.com>
2022-10-27 08:21:56 -0700

    simple strcpy function

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _strncpy - a function that copies a string into another.
@@ -13,35 +12,17 @@ char *_strncpy(char *str1, char *str2, int i)
 {
 	char *tmp = str1;
 
-	int len_str1 = 0, j = 0, u = 0, len_str2 = 0;
+	int u = 0;
 
-	while (str1[len_str1] != '\0')
-	{
-		len_str1++;
-	}
-
-	while (str2[len_str2] != '\0')
-		len_str2++;
-
-	if (i > len_str2)
-		i = len_str2;
-
-	while (str2[u] != str2[i])
+	while ((u < i && str2[u] != '\0'))
 	{
 		tmp[u] = str2[u];
 		u++;
-		j++;
 	}
-	if (j == len_str2)
+	while (u < i)
 	{
-		tmp[j] = '\0';
-		return (tmp);
+		tmp[u] = '\0';
+		u++;
 	}
-	while (tmp[j] != str2[len_str2])
-	{
-		tmp[j] = str1[j];
-		j++;
-	}
-	tmp[j] = '\0';
-	return (str1);
+	return (tmp);
 }
