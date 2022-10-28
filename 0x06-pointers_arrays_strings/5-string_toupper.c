d76ffa0304f3fa71ebc24674c4747fd12ea18f48
Oladunsi <okeoladunsi6@gmail.com>
2022-10-27 21:05:22 -0700

    documentation error corrected when declaring function name

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _string_toupper - a function that changes a string into Uppercase.
+ * string_toupper - a function that changes a string into Uppercase.
  * @str: an input string
  *
  * Return: A pointer to the resulting string
