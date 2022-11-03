944356f6c7f96e3d48589dab69ee97b428dbe422
Oladunsi <okeoladunsi6@gmail.com>
2022-11-02 23:30:03 -0700

    removed trailling whitespace on line 13 to suit betty

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,14 +3,14 @@
 /**
 * _strlen_recursion - a function that returns the length of a string.
 * @s: a char input of letters
-* 
+*
 * Return: An int length of a string
 */
 
 int _strlen_recursion(char *s)
 {
 	int count = 0;
-	
+
 	if (*s)
 	{
 		count += 1;
